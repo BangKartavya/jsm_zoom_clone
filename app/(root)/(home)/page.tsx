2347ca084eada878995862de670b2d2065ea6544
BangKartavya <bangkartavya@gmail.com>
2024-10-31 09:20:03 +0530

    fixed dates

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,16 +1,16 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  
+  const time = now.toLocaleString([],{hour:'2-digit',minute: '2-digit'});
   const date = (new Intl.DateTimeFormat([], { dateStyle: 'full' })).format(now);
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md: py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meetings at: 12:30 PM</h2>
           <div className='flex flex-col gap-2'>
             <h1 className="text-4xl font-extrabold lg:text-7xl">
               {time}
